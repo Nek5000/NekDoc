6c469b6bbc50198e071d5ed91c7665ee733adee8
yslan <r05246001@ntu.edu.tw>
2025-09-11 11:40:18 -0500

    Update sphinx version

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,7 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.8.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -178,4 +178,5 @@ epub_copyright = copyright
 epub_exclude_files = ['search.html']
 
 def setup(app):
-  app.add_stylesheet('css/custom.css')
+#  app.add_stylesheet('css/custom.css') # older API
+  app.add_css_file('css/custom.css')
